251246dec1ceaec34f1660c9b0c755e8190905d3
Aleksandr Menshikov <menshikov.abz@gmail.com>
2021-12-20 16:47:34 +0300

    Basic JS HW3 is solved

--- 1_BasicJS/3.arrays/task.js
+++ 1_BasicJS/3.arrays/task.js
@@ -1,8 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = true;
 
   // Ваш код
-
+  if (arr1.length === arr2.length){
+    for (let idx in arr1){
+      if (arr1[idx] !== arr2[idx]){
+        result = false;
+        break;
+      }
+    }
+  } else {
+    result = false;
+  }
   return result; // boolean
 }
 
@@ -10,6 +19,7 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+  resultArr = arr.filter((x) => x > 0 && x % 3 === 0);
+  resultArr = resultArr.map((x) => x * 10)
   return resultArr; // array
 }
