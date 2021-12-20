df26ff48964a59f13d61b7c52f814810e14b4103
Aleksandr Menshikov <menshikov.abz@gmail.com>
2021-12-20 17:45:32 +0300

    Basic JS HW4 is solved

--- 1_BasicJS/4.objects/task.js
+++ 1_BasicJS/4.objects/task.js
@@ -1,10 +1,38 @@
-function Student(name, gender, age) {
-    // Ваш код
+"use strict";
 
+function Student(name, gender, age) {
+  // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName
 }
 
 // ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  this.marks.push(mark);
+}
+Student.prototype.addMarks = function (...marks) {
+  for (let mark of marks){
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let reducer = (accumulator, curr) => accumulator + curr;
+  return this.marks.reduce(reducer) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  if (this.subject){
+    delete this.subject;
+  }
+  if (this.marks){
+    delete this.marks;
+  }
+  this.excluded = reason;
+}
