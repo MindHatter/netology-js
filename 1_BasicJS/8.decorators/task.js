b7f043cbaa794af901d56f2132c884ce51a3f973
Aleksandr Menshikov <menshikov.abz@gmail.com>
2021-12-21 15:25:23 +0300

    Basic JS HW8 is solved

--- 1_BasicJS/8.decorators/task.js
+++ 1_BasicJS/8.decorators/task.js
@@ -1,12 +1,40 @@
+"use strict";
+
 function cachingDecoratorNew(func) {
   // Ваш код
+  let cache = []
+  function wrapper(...args){
+    let hash = args.join();
+    let idx = cache.findIndex(x => x.hash === hash);
+    if (idx !== -1){
+
+      return `Из кэша: ${cache[idx].value}`;
+    }
+    let result = func(...args);
+    cache.push({hash:hash, value: result});
+    if (cache.length > 5){
+      cache.shift();
+    }
+    return `Вычисляем: ${result}`;
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
+function debounceDecoratorNew(func, ms) {
   // Ваш код
-}
+  let timeout = ms;
+  let flag = false;
+  let count = 0;
 
-function debounceDecorator2(func) {
-  // Ваш код
+  function wrapper (...args){
+    count += 1;
+    console.log(count);
+    if (!flag){
+      flag = true;
+      setTimeout(() => flag = false, ms);
+      return func(...args);
+    }
+  }
+  return wrapper;
 }
