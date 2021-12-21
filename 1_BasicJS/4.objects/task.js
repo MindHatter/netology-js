05fa8f9f621ec6d65a44f3b1fd12599ccabd1e00
Aleksandr Menshikov <menshikov.abz@gmail.com>
2021-12-21 11:07:27 +0300

    Basic JS HW4 fixed

--- 1_BasicJS/4.objects/task.js
+++ 1_BasicJS/4.objects/task.js
@@ -5,7 +5,7 @@ function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
-  this.marks = [];
+  this.marks;
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -14,9 +14,15 @@ Student.prototype.setSubject = function (subjectName) {
 
 // ваш код для остальных методов
 Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+    this.marks = [];
+  }
   this.marks.push(mark);
 }
 Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){
+    this.marks = [];
+  }
   for (let mark of marks){
     this.marks.push(mark);
   }
